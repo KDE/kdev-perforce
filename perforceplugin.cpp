6d5ece20eb71ffaa7ec2bf4bf9ea231d58a49705
Morten Volden <mvolden@gmail.com>
2010-11-21 23:03:53 +0100

    Initial commit

---
+++ perforceplugin.cpp
@@ -0,0 +1,303 @@
+/***************************************************************************
+ *   Copyright 2010  Morten Danielsen Volden                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "perforceplugin.h"
+
+#include <iostream>
+
+#include <KPluginFactory>
+#include <KPluginLoader>
+#include <KLocalizedString>
+#include <KAboutData>
+#include <KActionCollection>
+#include <QFileInfo>
+#include <QDir>
+#include <QProcessEnvironment>
+#include <QMenu>
+#include <kmessagebox.h>
+#include <vcs/vcsjob.h>
+#include <vcs/vcsrevision.h>
+#include <vcs/vcsevent.h>
+#include <vcs/dvcs/dvcsjob.h>
+#include <vcs/vcsannotation.h>
+#include <vcs/widgets/standardvcslocationwidget.h>
+
+#include <interfaces/context.h>
+#include <interfaces/contextmenuextension.h>
+#include <interfaces/icore.h>
+#include <interfaces/iruncontroller.h>
+
+
+
+#include <vcs/vcspluginhelper.h>
+
+using namespace KDevelop;
+
+//K_PLUGIN_FACTORY(KDevSvnFactory, registerPlugin<KDevSvnPlugin>();)
+//K_EXPORT_PLUGIN(KDevSvnFactory
+
+K_PLUGIN_FACTORY (KdevPerforceFactory, registerPlugin<perforceplugin>();)
+K_EXPORT_PLUGIN (KdevPerforceFactory (KAboutData ("kdevperforce","kdevperforce", ki18n ("Support for Perforce Version Control System"), "0.1", ki18n ("Support for Perforce Version Control System"), KAboutData::License_GPL)))
+
+perforceplugin::perforceplugin(QObject* parent, const QVariantList& ): 
+  KDevelop::IPlugin(KdevPerforceFactory::componentData(), parent )
+  , m_common(new KDevelop::VcsPluginHelper(this, this))
+  , m_perforcemenu( 0 )
+  , m_perforceConfigName("p4config.txt")
+  , m_edit_action( 0 )
+{
+    KDEV_USE_EXTENSION_INTERFACE( KDevelop::IBasicVersionControl )
+    KDEV_USE_EXTENSION_INTERFACE( KDevelop::ICentralizedVersionControl )
+    
+    QProcessEnvironment currentEviron(QProcessEnvironment::systemEnvironment());
+    // We will default search for p4config.txt - However if something else is used, search for that 
+    QString tmp(currentEviron.value("P4CONFIG"));
+    if(!tmp.isEmpty())
+    {
+	m_perforceConfigName = tmp;
+    }
+}
+
+perforceplugin::~perforceplugin()
+{
+}
+
+QString perforceplugin::name() const
+{
+  return i18n("Perforce");
+}
+
+KDevelop::VcsImportMetadataWidget* perforceplugin::createImportMetadataWidget(QWidget* /*parent*/)
+{
+  return 0;
+}
+
+
+bool perforceplugin::isVersionControlled(const KUrl& localLocation)
+{
+    const QFileInfo finfo(localLocation.toLocalFile());
+    QDir dir = finfo.isDir() ? QDir(localLocation.toLocalFile()) : finfo.absoluteDir();
+
+    do 
+    {
+      if(dir.exists(m_perforceConfigName))
+      {
+          return true;
+      }
+    } 
+    while (dir.cdUp()); 
+    return false;
+}
+
+KDevelop::VcsJob* perforceplugin::repositoryLocation(const KUrl& /*localLocation*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::add(const KUrl::List& localLocations, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
+{
+  /// Where should the job be run from?
+  QDir curDir(localLocations.front().toLocalFile());
+  DVcsJob* job = new DVcsJob(curDir, this, KDevelop::OutputJob::Verbose);
+  /// Im' pretty sure sanity checking is needed here.
+  *job << "p4" << "add" << localLocations;
+  
+  return job;
+}
+
+KDevelop::VcsJob* perforceplugin::remove(const KUrl::List& /*localLocations*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::copy(const KUrl& /*localLocationSrc*/, const KUrl& /*localLocationDstn*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::move(const KUrl& /*localLocationSrc*/, const KUrl& /*localLocationDst*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::status(const KUrl::List& /*localLocations*/, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::revert(const KUrl::List& localLocations, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
+{
+    if (localLocations.count() != 1) 
+    {
+        KMessageBox::error(0, i18n("Please select only one item for this operation"));
+        return 0; 
+    }
+
+    QFileInfo curFile(localLocations.front().toLocalFile());
+   
+    DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
+    setEnvironmentForJob(job, curFile);
+    *job << "p4" << "revert" << curFile.fileName();
+
+    return job;
+ 
+}
+
+KDevelop::VcsJob* perforceplugin::update(const KUrl::List& localLocations, const KDevelop::VcsRevision& /*rev*/, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
+{
+    QFileInfo curFile(localLocations.front().toLocalFile());
+   
+    DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
+    setEnvironmentForJob(job, curFile);
+    *job << "p4" << "-p" << "127.0.0.1:1666" << "info";
+
+    return job;
+}
+
+KDevelop::VcsJob* perforceplugin::commit(const QString& message, const KUrl::List& localLocations, KDevelop::IBasicVersionControl::RecursionMode recursion)
+{
+  QFileInfo curFile(localLocations.front().toLocalFile());
+   
+  DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
+  setEnvironmentForJob(job, curFile);
+  *job << "p4" << "submit" << "-d" << message << localLocations;
+  
+  return job;
+}
+
+KDevelop::VcsJob* perforceplugin::diff(const KUrl& /*fileOrDirectory*/, const KDevelop::VcsRevision& /*srcRevision*/, const KDevelop::VcsRevision& /*dstRevision*/, KDevelop::VcsDiff::Type , KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
+{
+    return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::log(const KUrl& /*localLocation*/, const KDevelop::VcsRevision& /*rev*/, long unsigned int /*limit*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::log(const KUrl& /*localLocation*/, const KDevelop::VcsRevision& /*rev*/, const KDevelop::VcsRevision& /*limit*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::annotate(const KUrl& /*localLocation*/, const KDevelop::VcsRevision& /*rev*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::resolve(const KUrl::List& /*localLocations*/, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::createWorkingCopy(const KDevelop::VcsLocation& /*sourceRepository*/, const KUrl& /*destinationDirectory*/, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
+{
+  return 0;
+}
+
+KDevelop::VcsLocationWidget* perforceplugin::vcsLocation(QWidget* parent) const
+{
+    return new StandardVcsLocationWidget(parent);
+}
+
+
+KDevelop::VcsJob* perforceplugin::edit(const KUrl& localLocation)
+{
+    QFileInfo curFile(localLocation.toLocalFile());
+   
+    DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
+    setEnvironmentForJob(job, curFile);
+    *job << "p4" << "edit" << curFile.fileName();
+
+    return job;
+}
+
+KDevelop::VcsJob* perforceplugin::unedit(const KUrl& /*localLocation*/)
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::localRevision(const KUrl& /*localLocation*/, KDevelop::VcsRevision::RevisionType )
+{
+  return 0;
+}
+
+KDevelop::VcsJob* perforceplugin::import(const QString& /*commitMessage*/, const KUrl& /*sourceDirectory*/, const KDevelop::VcsLocation& /*destinationRepository*/)
+{
+  return 0;
+}
+
+KDevelop::ContextMenuExtension perforceplugin::contextMenuExtension(KDevelop::Context* context)
+{
+    m_common->setupFromContext(context);
+
+    const KUrl::List & ctxUrlList  = m_common->contextUrlList();
+
+    bool hasVersionControlledEntries = false;
+    foreach(KUrl const& url, ctxUrlList) 
+    {
+        if (isVersionControlled(url)) 
+        {
+            hasVersionControlledEntries = true;
+            break;
+        }
+    }
+
+    //kDebug() << "version controlled?" << hasVersionControlledEntries;
+
+    if (!hasVersionControlledEntries)
+        return IPlugin::contextMenuExtension(context);
+
+    QMenu * perforceMenu = m_common->commonActions();
+    perforceMenu->addSeparator();    
+    
+    perforceMenu->addSeparator();
+    
+    if( !m_edit_action )
+    {
+        m_edit_action = new KAction(i18n("Edit"), this);
+        connect(m_edit_action, SIGNAL(triggered()), this, SLOT(ctxEdit()));
+    }
+    perforceMenu->addAction(m_edit_action);
+
+    ContextMenuExtension menuExt;
+    menuExt.addAction(ContextMenuExtension::VcsGroup, perforceMenu->menuAction());
+
+    return menuExt;
+}
+
+void perforceplugin::ctxEdit()
+{
+    KUrl::List const & ctxUrlList = m_common->contextUrlList();
+    if (ctxUrlList.count() != 1) 
+    {
+        KMessageBox::error(0, i18n("Please select only one item for this operation"));
+        return;
+    }
+    KUrl source = ctxUrlList.first();
+    KDevelop::ICore::self()->runController()->registerJob(edit(source));
+}
+
+void perforceplugin::setEnvironmentForJob(DVcsJob* job, const QFileInfo& curFile)
+{
+    KProcess* jobproc = job->process();
+    QStringList beforeEnv = jobproc->environment();
+    kDebug() << "Before setting the environment : " << beforeEnv;
+    jobproc->setEnv("P4CONFIG", m_perforceConfigName);
+    jobproc->setEnv("PWD", curFile.absolutePath());
+    QStringList afterEnv = jobproc->environment();  
+    kDebug() << "After setting the environment : " << afterEnv;   
+}
+
+
+
+
+
+
---
+++ perforceplugin.h
@@ -0,0 +1,143 @@
+/***************************************************************************
+ *   Copyright 2010  Morten Danielsen Volden                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KDEVPERFORCEPLUGIN_H
+#define KDEVPERFORCEPLUGIN_H
+
+#include <vcs/interfaces/icentralizedversioncontrol.h>
+#include <interfaces/iplugin.h>
+
+#include <QVariantList>
+#include <kaction.h>
+
+#include <KMimeType>
+#include <auto_ptr.h>
+
+class QMenu;
+class QFileInfo;
+
+
+namespace KDevelop
+{
+  class ContextMenuExtension;
+  class VcsPluginHelper;
+  class DVcsJob;
+}
+
+
+class perforceplugin : public KDevelop::IPlugin, public KDevelop::ICentralizedVersionControl
+{
+  Q_OBJECT
+  Q_INTERFACES(KDevelop::IBasicVersionControl KDevelop::ICentralizedVersionControl)
+
+public:
+  perforceplugin(QObject* parent, const QVariantList & = QVariantList()); 
+  virtual ~perforceplugin();
+  
+  //@{
+  /** Methods inherited from KDevelop::IBasicVersionControl */
+  QString name() const;
+  
+  KDevelop::VcsImportMetadataWidget* createImportMetadataWidget( QWidget* parent );
+  
+  bool isVersionControlled(const KUrl& localLocation);
+  
+  KDevelop::VcsJob* repositoryLocation(const KUrl& localLocation);
+  
+  KDevelop::VcsJob* add( const KUrl::List& localLocations,
+                         RecursionMode recursion = IBasicVersionControl::Recursive );
+  KDevelop::VcsJob* remove( const KUrl::List& localLocations );
+
+  KDevelop::VcsJob* copy( const KUrl& localLocationSrc,
+                          const KUrl& localLocationDstn );
+  KDevelop::VcsJob* move( const KUrl& localLocationSrc,
+                          const KUrl& localLocationDst );
+  KDevelop::VcsJob* status( const KUrl::List& localLocations,
+                            RecursionMode recursion = IBasicVersionControl::Recursive );
+
+  KDevelop::VcsJob* revert( const KUrl::List& localLocations,
+                            RecursionMode recursion = IBasicVersionControl::Recursive );
+
+  KDevelop::VcsJob* update( const KUrl::List& localLocations,
+                            const KDevelop::VcsRevision& rev = KDevelop::VcsRevision::createSpecialRevision( KDevelop::VcsRevision::Head ),
+                            KDevelop::IBasicVersionControl::RecursionMode recursion = KDevelop::IBasicVersionControl::Recursive );
+
+  KDevelop::VcsJob* commit( const QString& message,
+                            const KUrl::List& localLocations,
+                            KDevelop::IBasicVersionControl::RecursionMode recursion = KDevelop::IBasicVersionControl::Recursive );
+
+  KDevelop::VcsJob* diff( const KUrl& fileOrDirectory,
+                          const KDevelop::VcsRevision& srcRevision,
+                          const KDevelop::VcsRevision& dstRevision,
+                          KDevelop::VcsDiff::Type = KDevelop::VcsDiff::DiffUnified,
+                          KDevelop::IBasicVersionControl::RecursionMode recursion = KDevelop::IBasicVersionControl::Recursive );
+
+  KDevelop::VcsJob* log( const KUrl& localLocation,
+                         const KDevelop::VcsRevision& rev,
+                         unsigned long limit = 0 );
+
+  KDevelop::VcsJob* log( const KUrl& localLocation,
+                         const KDevelop::VcsRevision& rev = KDevelop::VcsRevision::createSpecialRevision( KDevelop::VcsRevision::Base ),
+                         const KDevelop::VcsRevision& limit= KDevelop::VcsRevision::createSpecialRevision( KDevelop::VcsRevision::Start ) );
+
+  KDevelop::VcsJob* annotate( const KUrl& localLocation,
+                              const KDevelop::VcsRevision& rev = KDevelop::VcsRevision::createSpecialRevision( KDevelop::VcsRevision::Head ) );
+
+  KDevelop::VcsJob* resolve( const KUrl::List& localLocations,
+                             KDevelop::IBasicVersionControl::RecursionMode recursion );
+
+  KDevelop::VcsJob* createWorkingCopy(const  KDevelop::VcsLocation & sourceRepository, 
+                                      const KUrl & destinationDirectory, 
+                                      KDevelop::IBasicVersionControl::RecursionMode recursion = IBasicVersionControl::Recursive);
+
+    
+  KDevelop::VcsLocationWidget* vcsLocation(QWidget* parent) const;  
+  //@}
+  
+  //@{
+  /** Methods inherited from KDevelop::ICentralizedVersionControl  */
+    KDevelop::VcsJob* edit( const KUrl& localLocation );
+
+    KDevelop::VcsJob* unedit( const KUrl& localLocation );
+
+    KDevelop::VcsJob* localRevision( const KUrl& localLocation,
+                                    KDevelop::VcsRevision::RevisionType );
+
+    KDevelop::VcsJob* import(const QString & commitMessage, 
+                             const KUrl & sourceDirectory, 
+                             const  KDevelop::VcsLocation & destinationRepository);
+  //@}  
+  
+    KDevelop::ContextMenuExtension contextMenuExtension(KDevelop::Context* context);
+  
+  
+public Q_SLOTS:
+
+  /// invoked by context-menu
+  void ctxEdit();
+//   void ctxUnedit();
+//   void ctxLocalRevision();
+//   void ctxImport();
+
+
+private:
+    bool pathHasConfigFile(const KUrl & dirPath);
+    void setEnvironmentForJob(KDevelop::DVcsJob* job, QFileInfo const& fsObject);
+  
+    std::auto_ptr<KDevelop::VcsPluginHelper> m_common;
+    QMenu* m_perforcemenu;
+    QString m_perforceConfigName;
+    KAction* m_edit_action;
+
+
+
+};
+
+#endif // PERFORCEPLUGIN_H
