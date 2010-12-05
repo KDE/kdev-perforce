967e4305e25f9283773d28665c6569361e577fd8
Morten Volden <mvolden@gmail.com>
2010-12-05 23:07:46 +0100

    Improve on the status function.

--- perforceplugin.cpp
+++ perforceplugin.cpp
@@ -40,30 +40,34 @@
 
 using namespace KDevelop;
 
+namespace
+{
+const QString ACTION_STR("... action ");
+}
 
-/* Todo: 
- * 			
+/* Todo:
+ *
  * 			Implement diff to make commit work */
 
 K_PLUGIN_FACTORY (KdevPerforceFactory, registerPlugin<perforceplugin>();)
 K_EXPORT_PLUGIN (KdevPerforceFactory (KAboutData ("kdevperforce","kdevperforce", ki18n ("Support for Perforce Version Control System"), "0.1", ki18n ("Support for Perforce Version Control System"), KAboutData::License_GPL)))
 
-perforceplugin::perforceplugin(QObject* parent, const QVariantList& ): 
-  KDevelop::IPlugin(KdevPerforceFactory::componentData(), parent )
-  , m_common(new KDevelop::VcsPluginHelper(this, this))
-  , m_perforcemenu( 0 )
-  , m_perforceConfigName("p4config.txt")
-  , m_edit_action( 0 )
+perforceplugin::perforceplugin(QObject* parent, const QVariantList& ):
+        KDevelop::IPlugin(KdevPerforceFactory::componentData(), parent )
+        , m_common(new KDevelop::VcsPluginHelper(this, this))
+        , m_perforcemenu( 0 )
+        , m_perforceConfigName("p4config.txt")
+        , m_edit_action( 0 )
 {
     KDEV_USE_EXTENSION_INTERFACE( KDevelop::IBasicVersionControl )
     KDEV_USE_EXTENSION_INTERFACE( KDevelop::ICentralizedVersionControl )
-    
+
     QProcessEnvironment currentEviron(QProcessEnvironment::systemEnvironment());
-    // We will default search for p4config.txt - However if something else is used, search for that 
+    // We will default search for p4config.txt - However if something else is used, search for that
     QString tmp(currentEviron.value("P4CONFIG"));
-    if(!tmp.isEmpty())
+    if (!tmp.isEmpty())
     {
-	m_perforceConfigName = tmp;
+        m_perforceConfigName = tmp;
     }
 }
 
@@ -73,12 +77,12 @@ perforceplugin::~perforceplugin()
 
 QString perforceplugin::name() const
 {
-  return i18n("Perforce");
+    return i18n("Perforce");
 }
 
 KDevelop::VcsImportMetadataWidget* perforceplugin::createImportMetadataWidget(QWidget* /*parent*/)
 {
-  return 0;
+    return 0;
 }
 
 bool perforceplugin::isValidDirectory(const KUrl & dirPath)
@@ -86,21 +90,21 @@ bool perforceplugin::isValidDirectory(const KUrl & dirPath)
     const QFileInfo finfo(dirPath.toLocalFile());
     QDir dir = finfo.isDir() ? QDir(dirPath.toLocalFile()) : finfo.absoluteDir();
 
-    do 
+    do
     {
-      if(dir.exists(m_perforceConfigName))
-      {
-          return true;
-      }
-    } 
-    while (dir.cdUp()); 
+        if (dir.exists(m_perforceConfigName))
+        {
+            return true;
+        }
+    }
+    while (dir.cdUp());
     return false;
 }
 
 bool perforceplugin::isVersionControlled(const KUrl& localLocation)
 {
     QFileInfo fsObject(localLocation.toLocalFile());
-    if (fsObject.isDir()) 
+    if (fsObject.isDir())
     {
         return isValidDirectory(localLocation);
     }
@@ -120,9 +124,9 @@ bool perforceplugin::parseP4fstat(const QFileInfo& curFile, OutputJob::OutputJob
     QScopedPointer<DVcsJob> job(p4fstatJob(curFile, verbosity));
     if (job->exec() && job->status() == KDevelop::VcsJob::JobSucceeded)
     {
-      kDebug() << "Perforce returned: " << job->output();
-      if(!job->output().isEmpty())
-	return true;
+        kDebug() << "Perforce returned: " << job->output();
+        if (!job->output().isEmpty())
+            return true;
     }
     return false;
 }
@@ -130,44 +134,44 @@ bool perforceplugin::parseP4fstat(const QFileInfo& curFile, OutputJob::OutputJob
 
 KDevelop::VcsJob* perforceplugin::repositoryLocation(const KUrl& /*localLocation*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::add(const KUrl::List& localLocations, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
 {
-  QFileInfo curFile(localLocations.front().toLocalFile());
-  DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
-  setEnvironmentForJob(job, curFile);
-  *job << "p4" << "add" << localLocations;
-  
-  return job;
+    QFileInfo curFile(localLocations.front().toLocalFile());
+    DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
+    setEnvironmentForJob(job, curFile);
+    *job << "p4" << "add" << localLocations;
+
+    return job;
 }
 
 KDevelop::VcsJob* perforceplugin::remove(const KUrl::List& /*localLocations*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::copy(const KUrl& /*localLocationSrc*/, const KUrl& /*localLocationDstn*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::move(const KUrl& /*localLocationSrc*/, const KUrl& /*localLocationDst*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::status(const KUrl::List& localLocations, KDevelop::IBasicVersionControl::RecursionMode recursion)
 {
-    if (localLocations.count() != 1) 
+    if (localLocations.count() != 1)
     {
         KMessageBox::error(0, i18n("Please select only one item for this operation"));
-        return 0; 
+        return 0;
     }
 
     QFileInfo curFile(localLocations.front().toLocalFile());
-   
+
     DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
     setEnvironmentForJob(job, curFile);
     *job << "p4" << "fstat" << curFile.fileName();
@@ -178,26 +182,26 @@ KDevelop::VcsJob* perforceplugin::status(const KUrl::List& localLocations, KDeve
 
 KDevelop::VcsJob* perforceplugin::revert(const KUrl::List& localLocations, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
 {
-    if (localLocations.count() != 1) 
+    if (localLocations.count() != 1)
     {
         KMessageBox::error(0, i18n("Please select only one item for this operation"));
-        return 0; 
+        return 0;
     }
 
     QFileInfo curFile(localLocations.front().toLocalFile());
-   
+
     DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
     setEnvironmentForJob(job, curFile);
     *job << "p4" << "revert" << curFile.fileName();
 
     return job;
- 
+
 }
 
 KDevelop::VcsJob* perforceplugin::update(const KUrl::List& localLocations, const KDevelop::VcsRevision& /*rev*/, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
 {
     QFileInfo curFile(localLocations.front().toLocalFile());
-   
+
     DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
     setEnvironmentForJob(job, curFile);
     *job << "p4" << "-p" << "127.0.0.1:1666" << "info";
@@ -207,23 +211,23 @@ KDevelop::VcsJob* perforceplugin::update(const KUrl::List& localLocations, const
 
 KDevelop::VcsJob* perforceplugin::commit(const QString& message, const KUrl::List& localLocations, KDevelop::IBasicVersionControl::RecursionMode recursion)
 {
-  if (localLocations.empty() || message.isEmpty())
-     return errorsFound(i18n("No files or message specified"));
+    if (localLocations.empty() || message.isEmpty())
+        return errorsFound(i18n("No files or message specified"));
 
-  
-  QFileInfo curFile(localLocations.front().toLocalFile());
-   
-  DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
-  setEnvironmentForJob(job, curFile);
-  *job << "p4" << "submit" << "-d" << message << localLocations;
-  
-  return job;
+
+    QFileInfo curFile(localLocations.front().toLocalFile());
+
+    DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
+    setEnvironmentForJob(job, curFile);
+    *job << "p4" << "submit" << "-d" << message << localLocations;
+
+    return job;
 }
 
 KDevelop::VcsJob* perforceplugin::diff(const KUrl& fileOrDirectory, const KDevelop::VcsRevision& /*srcRevision*/, const KDevelop::VcsRevision& /*dstRevision*/, KDevelop::VcsDiff::Type , KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
 {
     QFileInfo curFile(fileOrDirectory.toLocalFile());
-   
+
     DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
     setEnvironmentForJob(job, curFile);
     *job << "p4" << "diff" << "-du" << curFile.fileName();
@@ -234,27 +238,27 @@ KDevelop::VcsJob* perforceplugin::diff(const KUrl& fileOrDirectory, const KDevel
 
 KDevelop::VcsJob* perforceplugin::log(const KUrl& /*localLocation*/, const KDevelop::VcsRevision& /*rev*/, long unsigned int /*limit*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::log(const KUrl& /*localLocation*/, const KDevelop::VcsRevision& /*rev*/, const KDevelop::VcsRevision& /*limit*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::annotate(const KUrl& /*localLocation*/, const KDevelop::VcsRevision& /*rev*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::resolve(const KUrl::List& /*localLocations*/, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::createWorkingCopy(const KDevelop::VcsLocation& /*sourceRepository*/, const KUrl& /*destinationDirectory*/, KDevelop::IBasicVersionControl::RecursionMode /*recursion*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsLocationWidget* perforceplugin::vcsLocation(QWidget* parent) const
@@ -266,7 +270,7 @@ KDevelop::VcsLocationWidget* perforceplugin::vcsLocation(QWidget* parent) const
 KDevelop::VcsJob* perforceplugin::edit(const KUrl& localLocation)
 {
     QFileInfo curFile(localLocation.toLocalFile());
-   
+
     DVcsJob* job = new DVcsJob(curFile.dir(), this, KDevelop::OutputJob::Verbose);
     setEnvironmentForJob(job, curFile);
     *job << "p4" << "edit" << curFile.fileName();
@@ -276,17 +280,17 @@ KDevelop::VcsJob* perforceplugin::edit(const KUrl& localLocation)
 
 KDevelop::VcsJob* perforceplugin::unedit(const KUrl& /*localLocation*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::localRevision(const KUrl& /*localLocation*/, KDevelop::VcsRevision::RevisionType )
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::VcsJob* perforceplugin::import(const QString& /*commitMessage*/, const KUrl& /*sourceDirectory*/, const KDevelop::VcsLocation& /*destinationRepository*/)
 {
-  return 0;
+    return 0;
 }
 
 KDevelop::ContextMenuExtension perforceplugin::contextMenuExtension(KDevelop::Context* context)
@@ -296,9 +300,9 @@ KDevelop::ContextMenuExtension perforceplugin::contextMenuExtension(KDevelop::Co
     const KUrl::List & ctxUrlList  = m_common->contextUrlList();
 
     bool hasVersionControlledEntries = false;
-    foreach(KUrl const& url, ctxUrlList) 
+    foreach(KUrl const& url, ctxUrlList)
     {
-        if (isValidDirectory(url)) 
+        if (isValidDirectory(url))
         {
             hasVersionControlledEntries = true;
             break;
@@ -311,11 +315,11 @@ KDevelop::ContextMenuExtension perforceplugin::contextMenuExtension(KDevelop::Co
         return IPlugin::contextMenuExtension(context);
 
     QMenu * perforceMenu = m_common->commonActions();
-    perforceMenu->addSeparator();    
-    
     perforceMenu->addSeparator();
-    
-    if( !m_edit_action )
+
+    perforceMenu->addSeparator();
+
+    if ( !m_edit_action )
     {
         m_edit_action = new KAction(i18n("Edit"), this);
         connect(m_edit_action, SIGNAL(triggered()), this, SLOT(ctxEdit()));
@@ -331,7 +335,7 @@ KDevelop::ContextMenuExtension perforceplugin::contextMenuExtension(KDevelop::Co
 void perforceplugin::ctxEdit()
 {
     KUrl::List const & ctxUrlList = m_common->contextUrlList();
-    if (ctxUrlList.count() != 1) 
+    if (ctxUrlList.count() != 1)
     {
         KMessageBox::error(0, i18n("Please select only one item for this operation"));
         return;
@@ -347,8 +351,8 @@ void perforceplugin::setEnvironmentForJob(DVcsJob* job, const QFileInfo& curFile
     //kDebug() << "Before setting the environment : " << beforeEnv;
     jobproc->setEnv("P4CONFIG", m_perforceConfigName);
     jobproc->setEnv("PWD", curFile.absolutePath());
-    QStringList afterEnv = jobproc->environment();  
-    //kDebug() << "After setting the environment : " << afterEnv;   
+    QStringList afterEnv = jobproc->environment();
+    //kDebug() << "After setting the environment : " << afterEnv;
 }
 
 void perforceplugin::parseP4StatusOutput(DVcsJob* job)
@@ -357,23 +361,34 @@ void perforceplugin::parseP4StatusOutput(DVcsJob* job)
     KUrl fileUrl = job->directory().absolutePath();
     QVariantList statuses;
     QList<KUrl> processedFiles;
-    
-    
-    foreach(const QString& line, outputLines) 
+
+
+    VcsStatusInfo status;
+    status.setUrl(fileUrl);
+    status.setState(VcsStatusInfo::ItemUserState);
+    foreach(const QString& line, outputLines)
     {
-	int idx(line.indexOf("no such file(s)"));
-	if(idx != -1)
-	{
-	    QString curr = line.left(line.size()-idx-15);
-	    fileUrl.addPath(curr);
-            VcsStatusInfo status;
-            status.setUrl(fileUrl);
-            status.setState(VcsStatusInfo::ItemUnknown);
-            
-            statuses.append(qVariantFromValue<VcsStatusInfo>(status));
-	}
-        //kDebug() << "Checking perforce status for " << line << curr << messageToState(state);
+        int idx(line.indexOf(ACTION_STR));
+        if (idx != -1)
+        {
+            QString curr = line.right(line.size() - ACTION_STR.size());
+            kDebug() << "PARSED FROM P4 FSTAT JOB " << curr;
+
+            if (curr == "edit")
+            {
+                status.setState(VcsStatusInfo::ItemModified);
+            }
+            else if (curr == "add")
+            {
+                status.setState(VcsStatusInfo::ItemAdded);
+            }
+            else
+            {
+                status.setState(VcsStatusInfo::ItemUserState);
+            }
+        }
     }
+    statuses.append(qVariantFromValue<VcsStatusInfo>(status));
     job->setResults(statuses);
 }
 
@@ -381,20 +396,20 @@ void perforceplugin::parseP4DiffOutput(DVcsJob* job)
 {
     VcsDiff diff;
     diff.setDiff(job->output());
-    
+
     QDir dir(job->directory());
 
-    do 
+    do
     {
-      if(dir.exists(m_perforceConfigName))
-      {
-          break;
-      }
-    } 
-    while (dir.cdUp()); 
+        if (dir.exists(m_perforceConfigName))
+        {
+            break;
+        }
+    }
+    while (dir.cdUp());
 
     diff.setBaseDiff(KUrl(dir.absolutePath()));
-    
+
     job->setResults(qVariantFromValue(diff));
 }
 
