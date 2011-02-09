d3fe8f58a927d55c9cfe56b96b0c27ac221d012c
Morten Volden <mvolden@gmail.com>
2011-02-09 22:18:14 +0100

    Fix license mess

--- test/perforceplugintest.cpp
+++ test/perforceplugintest.cpp
@@ -1,11 +1,22 @@
 /***************************************************************************
- *   Copyright 2011  Morten Danielsen Volden                               *
+ *   This file was inspired by KDevelop's git plugin                       *
+ *   Copyright 2008 Evgeniy Ivanov <powerfox@kde.ru>                       *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
+ *   Adapted for Perforce                                                  *
+ *   Copyright 2011  Morten Danielsen Volden <mvolden2@gmail.com>          *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation, either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  ***************************************************************************/
 
 #include "perforceplugintest.h"
