426ad28ebee315f03fdd69e6984cac8b514fb0cd
Kevin Funk <kfunk@kde.org>
2016-01-03 21:51:59 +0100

    Rename: perforceplugintest -> test_perforce

--- test/test_perforce.cpp
+++ test/test_perforce.cpp
@@ -19,12 +19,9 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  ***************************************************************************/
 
-#include "perforceplugintest.h"
-
+#include "test_perforce.h"
 
 #include <QtTest/QtTest>
-
-//#include <qtest_kde.h>
 
 #include <tests/autotestshell.h>
 #include <tests/testcore.h>
